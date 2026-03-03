40830126ddd2a335f5508bf88ab5fbdf24794dd9
Deno von Selasinsky <dvselas@vselas.de>
2026-03-03 17:54:22 +0100

    Revert "Switch from HLS to WebRTC streaming via go2rtc"
    This reverts commit 164670532268f091ad901742139323e4a9fdba16.

--- custom_components/unifi_protect/camera.py
+++ custom_components/unifi_protect/camera.py
@@ -66,9 +66,9 @@ class ProtectCameraEntity(CoordinatorEntity[ProtectDataUpdateCoordinator], Camer
         self._attr_device_info = camera.device_info
         # Set name to None so entity uses device name (not "device_name None")
         self._attr_name = None
-        # Enable streaming support with WebRTC (go2rtc handles RTSPS → WebRTC)
+        # Enable streaming support
         self._attr_supported_features = CameraEntityFeature.STREAM
-        self._attr_frontend_stream_type = "web_rtc"
+        self._attr_frontend_stream_type = "hls"
 
     @property
     def camera(self) -> ProtectCamera:
